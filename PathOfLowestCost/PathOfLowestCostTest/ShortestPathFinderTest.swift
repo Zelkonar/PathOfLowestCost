e60da517e895fe5e2d12a59037020d0c7758214f
Jeffrey Wolf <j.j.wolf2015@gmail.com>
2016-07-06 22:45:52 -0400

    Refactor Tests
    main class being tested moved out to class variable, setup function not
    yet needed.

--- PathOfLowestCost/PathOfLowestCostTest/ShortestPathFinderTest.swift
+++ PathOfLowestCost/PathOfLowestCostTest/ShortestPathFinderTest.swift
@@ -1,26 +1,21 @@
 import XCTest
 
 class ShortestPathFinderTest: XCTestCase {
+  var shortestPathFinder = ShortestPathFinder()
   
   func testAddRowASimpleRow() {
-    let shortestPathFinder = ShortestPathFinder()
-    
     shortestPathFinder.addRow("1")
     
     XCTAssertEqual(shortestPathFinder.board, [1])
   }
   
   func testAddRowWithTwoValues() {
-    let shortestPathFinder = ShortestPathFinder()
-    
     shortestPathFinder.addRow("2 3")
     
     XCTAssertEqual(shortestPathFinder.board, [2, 3])
   }
   
   func testAddRowUpdatesRowAttribute() {
-    let shortestPathFinder = ShortestPathFinder()
-    
     shortestPathFinder.addRow("2 3")
     
     XCTAssertEqual(shortestPathFinder.rows, 1)

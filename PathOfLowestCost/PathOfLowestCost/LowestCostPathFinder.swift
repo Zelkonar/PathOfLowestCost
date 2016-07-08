64ffc5dd2fd7127d7603ab66f6002c743711646e
Jeffrey Wolf <j.j.wolf2015@gmail.com>
2016-07-07 22:14:18 -0400

    lowestCostPathFinder can initialize with a board
    LowestCostPathFinder should necessitate Tile, not make Tile first for
    ‘proper’ TDD

--- PathOfLowestCost/PathOfLowestCost/LowestCostPathFinder.swift
+++ PathOfLowestCost/PathOfLowestCost/LowestCostPathFinder.swift
@@ -2,4 +2,8 @@ import Foundation
 
 class LowestCostPathFinder{
   var board = Board()
+  
+  init(board: Board){
+    self.board = board
+  }
 }
--- PathOfLowestCost/PathOfLowestCostTest/LowestCostPathFinderTest.swift
+++ PathOfLowestCost/PathOfLowestCostTest/LowestCostPathFinderTest.swift
@@ -1,4 +1,14 @@
 import XCTest
 
 class LowestCostPathFinderTest: XCTestCase {
+  
+  func testLowestCostPathFinderHasInitializerForBoard(){
+    let board = Board()
+    board.addRow("1")
+    board.addRow("2")
+    
+    let lowestCostPathFinder = LowestCostPathFinder(board: board)
+    
+    XCTAssert(lowestCostPathFinder.board === board)
+  }
 }

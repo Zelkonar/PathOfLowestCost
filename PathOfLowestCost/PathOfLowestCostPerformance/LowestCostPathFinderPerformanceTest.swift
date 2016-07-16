9fa5fff1cd480fdb13c41d007d859ead86f8b4ca
Jeffrey Wolf <j.j.wolf2015@gmail.com>
2016-07-16 15:19:23 -0400

    Refactor Performance: DRY

--- PathOfLowestCost/PathOfLowestCostPerformance/LowestCostPathFinderPerformanceTest.swift
+++ PathOfLowestCost/PathOfLowestCostPerformance/LowestCostPathFinderPerformanceTest.swift
@@ -1,39 +1,33 @@
 import XCTest
 
 class LowestCostPathFinderPerformanceTest: XCTestCase {
-  
+  var lowestCostPathFinder = LowestCostPathFinder()
+  var board = Board()
+
   func testMediumBoard() {
-    let lowestCostPathFinder = LowestCostPathFinder()
-    let board = Board()
-    var rowString = String()
-    for column in 1...50{
-      rowString += column % 2 == 0 ? "10 " : "-10 "
-    }
-    rowString = String(rowString.characters.dropLast())
-    for _ in 1...5{
-      board.addRow(rowString)
-    }
+    createBoard(5, 50)
     lowestCostPathFinder.board = board
     self.measureBlock {
-      lowestCostPathFinder.findLowestCostPath()
+      self.lowestCostPathFinder.findLowestCostPath()
     }
   }
   
   func testHugeBoard() {
-    let lowestCostPathFinder = LowestCostPathFinder()
-    let board = Board()
-    var rowString = String()
-    for column in 1...100{
-      rowString += column % 2 == 0 ? "10 " : "-10 "
-    }
-    rowString = String(rowString.characters.dropLast())
-    for _ in 1...10{
-      board.addRow(rowString)
-    }
+    createBoard(10, 100)
     lowestCostPathFinder.board = board
     self.measureBlock {
-      lowestCostPathFinder.findLowestCostPath()
+      self.lowestCostPathFinder.findLowestCostPath()
     }
   }
   
+  private func createBoard(rows: Int, _ columns: Int){
+    var rowString = String()
+    for column in 1...columns{
+      rowString += column % 2 == 0 ? "10 " : "-10 "
+    }
+    rowString = String(rowString.characters.dropLast())
+    for _ in 1...rows{
+      board.addRow(rowString)
+    }
+  }
 }

3210859c1aad3fcaae529a425a9c7f8cc13a56ed
Jeffrey Wolf <j.j.wolf2015@gmail.com>
2016-07-16 15:00:22 -0400

    Add huge board performance test, set performance project to disabled
    disabled since they should only be manually run, not a functional test.
    at this point of the program, that performance test would not finish in
    our lifetime (ran for 25 minutes, trying to do an estimated something
    about 1-2 quindecillion operations…)

--- PathOfLowestCost/PathOfLowestCostPerformance/LowestCostPathFinderPerformanceTest.swift
+++ PathOfLowestCost/PathOfLowestCostPerformance/LowestCostPathFinderPerformanceTest.swift
@@ -1,5 +1,21 @@
 import XCTest
 
 class LowestCostPathFinderPerformanceTest: XCTestCase {
-
+  func testHugeBoard() {
+    let lowestCostPathFinder = LowestCostPathFinder()
+    let board = Board()
+    var rowString = String()
+    for column in 1...100{
+      rowString += column % 2 == 0 ? "10 " : "-10 "
+    }
+    rowString = String(rowString.characters.dropLast())
+    for _ in 1...10{
+      board.addRow(rowString)
+    }
+    lowestCostPathFinder.board = board
+    self.measureBlock {
+      lowestCostPathFinder.findLowestCostPath()
+    }
+  }
+  
 }

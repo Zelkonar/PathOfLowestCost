f72987119088eea464b436f3a6704a9ee7632f6a
Jeffrey Wolf <j.j.wolf2015@gmail.com>
2016-07-16 15:09:43 -0400

    Performance: medium sized board
    this also times out :(

--- PathOfLowestCost/PathOfLowestCostPerformance/LowestCostPathFinderPerformanceTest.swift
+++ PathOfLowestCost/PathOfLowestCostPerformance/LowestCostPathFinderPerformanceTest.swift
@@ -1,6 +1,24 @@
 import XCTest
 
 class LowestCostPathFinderPerformanceTest: XCTestCase {
+  
+  func testMediumBoard() {
+    let lowestCostPathFinder = LowestCostPathFinder()
+    let board = Board()
+    var rowString = String()
+    for column in 1...50{
+      rowString += column % 2 == 0 ? "10 " : "-10 "
+    }
+    rowString = String(rowString.characters.dropLast())
+    for _ in 1...5{
+      board.addRow(rowString)
+    }
+    lowestCostPathFinder.board = board
+    self.measureBlock {
+      lowestCostPathFinder.findLowestCostPath()
+    }
+  }
+  
   func testHugeBoard() {
     let lowestCostPathFinder = LowestCostPathFinder()
     let board = Board()

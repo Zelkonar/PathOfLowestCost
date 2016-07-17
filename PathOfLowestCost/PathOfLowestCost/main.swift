9b4cee568dbdff18ecec446f5692ff89f0f541d1
Jeffrey Wolf <j.j.wolf2015@gmail.com>
2016-07-17 18:45:57 -0400

    Project Files and User State
    Open up to a non-com-jumbled XCode. Directions in main mirroring that
    in README.md

--- PathOfLowestCost/PathOfLowestCost/main.swift
+++ PathOfLowestCost/PathOfLowestCost/main.swift
@@ -1,1 +1,12 @@
 import Foundation
+
+/***********************************************************************************
+
+ No main, product functionality defined via tests
+ 
+ Set active scheme above to either PathOfLowestCostTest
+ or to PathOfLowestCostPerformance.
+ 
+ Run either in the test navigator or ⌘ + U once the scheme has been set appropiately
+ 
+ ***********************************************************************************/

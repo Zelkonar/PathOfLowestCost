895ef2a99caed3c49f2904cbe471a7f4a2da7703
Jeffrey Wolf <j.j.wolf2015@gmail.com>
2016-07-17 11:03:07 -0400

    Refactor: Extraneous parentheses

--- PathOfLowestCost/PathOfLowestCost/LowestCostPathFinder.swift
+++ PathOfLowestCost/PathOfLowestCost/LowestCostPathFinder.swift
@@ -22,7 +22,7 @@ class LowestCostPathFinder{
         paths.append(pathToAdd(j, i, paths))
       }
       let traversalCount = maxTraverseCount(paths)
-      paths = paths.filter({$0.pathOfLowestCost.count == traversalCount})
+      paths = paths.filter{$0.pathOfLowestCost.count == traversalCount}
       if (traversalCount < i){
         break
       }
@@ -48,7 +48,7 @@ class LowestCostPathFinder{
   }
   
   private func getUniqueTile(row row: Int, column: Int) -> Tile{
-    return board!.tiles.filter({$0.row == row && $0.column == column}).first!
+    return board!.tiles.filter{$0.row == row && $0.column == column}.first!
   }
   
   private func isValidToContinue(pathValue: Int, _ tileValue: Int) -> Bool{
@@ -75,18 +75,18 @@ class LowestCostPathFinder{
   }
   
   private func maxTraverseCount(paths: [Path]) -> Int{
-    return paths.maxElement({$0.0.pathOfLowestCost.count <= $0.1.pathOfLowestCost.count})!.pathOfLowestCost.count
+    return paths.maxElement{$0.0.pathOfLowestCost.count <= $0.1.pathOfLowestCost.count}!.pathOfLowestCost.count
   }
   
   private func pathOfLowestCost(paths: [Path]) -> Path{
-    return paths.minElement({$0.0.lowestTotalCost < $0.1.lowestTotalCost})!
+    return paths.minElement{$0.0.lowestTotalCost < $0.1.lowestTotalCost}!
   }
   
   private func lowestCostPathBeforeTile(paths: [Path], _ tile: Tile, _ column: Int) -> Path{
     let rowPlusOne = tile.row + 1 > board?.rows ? 1 : tile.row + 1
     let rowMinusOne = tile.row - 1 < 1 ? board!.rows : tile.row - 1
     let rowsToAdd = [tile.row, rowPlusOne, rowMinusOne]
-    let possiblePaths = paths.filter({$0.pathOfLowestCost.count == column - 1 && rowsToAdd.contains($0.pathOfLowestCost.last!)})
+    let possiblePaths = paths.filter{$0.pathOfLowestCost.count == column - 1 && rowsToAdd.contains($0.pathOfLowestCost.last!)}
     return pathOfLowestCost(possiblePaths)
   }
   
